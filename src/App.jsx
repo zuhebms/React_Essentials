5d836fa95af9ac5a841ad0b9c7e75fa4e485ef4b
zuhebms <83272858+zuhebms@users.noreply.github.com>
2023-12-30 15:53:26 +0530

    added content to the tab buttons with the help of data.js using objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import CoreConcept from "./components/CoreConcepts.jsx";
-import Header from './components/Header.jsx';
-import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
+import { CORE_CONCEPTS,EXAMPLES } from "./data.js";
 import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
- 
 function App() {
-  
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components','jsx','props','state'
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
   return (
     <div>
       <Header />
@@ -25,10 +32,25 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {/* conditional jsx rendering  */}
+          {!selectedTopic && "Please select a topic"}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title} </h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
-        
       </main>
     </div>
   );
